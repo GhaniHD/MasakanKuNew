569599fe5fd0e3579db858628b9c194f3f16585f
GhaniHD <120998349+GhaniHD@users.noreply.github.com>
2026-04-30 20:52:19 +0700

    fix appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // ← tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ← tambahkan ini
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
